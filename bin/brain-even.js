fc27df1f77b4cde9ba1296fa05b57d37a182be94
Diana Shilova <Shilova_de@mail.ru>
2022-04-15 15:11:32 +0300

    Create brain-even lesson 5

---
+++ bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import
+{ isEven } from '../src/cli-even.js';
+
+isEven();
---
+++ src/cli-even.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+// eslint-disable-next-line import/prefer-default-export
+export function isEven() {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${name}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  let i = 1;
+  while (i <= 3) {
+    const randomNumber = Math.round(Math.floor(Math.random() * 100));
+    let answerComputer;
+    if (randomNumber % 2 === 0) {
+      answerComputer = 'yes';
+    } else { answerComputer = 'no'; }
+    console.log(`Question: ${randomNumber}`);
+    const answerUser = readlineSync.question('Your answer: ');
+    if (answerUser === answerComputer) {
+      console.log('Correct!');
+      i += 1;
+    } else console.log(`Let,s try again, ${name}`);
+  }
+  console.log(`Congratulations, ${name}`);
+}
