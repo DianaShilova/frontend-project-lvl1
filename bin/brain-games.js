e58ecbb23505d7b2bc7151c38ad9e96e5f9c6443
Diana Shilova <Shilova_de@mail.ru>
2022-05-07 09:16:33 +0300

    delete excess file

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import greetings from '../src/game.js';
+import greetings from '../src/cli.js';
 
 greetings();
--- src/cli.js
+++ src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
-// eslint-disable-next-line import/prefer-default-export
-export function greetings() {
+
+const greetings = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${name}!`);
-}
+};
+export default greetings;
